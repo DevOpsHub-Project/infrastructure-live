f33a514efe9f509eba15b2575826846bd7841699
svishal16 <shrivastavavishal640@gmail.com>
2025-09-18 18:48:32 +0530

    Delete infrastructure-modules directory

--- infrastructure-modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "eks_cluster_role_arn" {
-	description = "EKS cluster role ARN"
-	value       = aws_iam_role.eks_cluster.arn
-}
-
-output "eks_node_group_role_arn" {
-	description = "EKS node group role ARN"
-	value       = aws_iam_role.eks_node_group.arn
-}
--- infrastructure-modules/iam/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "name_prefix" {
-	description = "Prefix for IAM resource names"
-	type        = string
-}
